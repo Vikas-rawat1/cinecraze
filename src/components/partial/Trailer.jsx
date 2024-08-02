9d4669b1a5719142248d209e108b1d937d3dd6cb
vikas <rawatvikas689@gmail.com>
2024-08-03 01:09:42 +0530

    commit

--- src/components/partial/Trailer.jsx
+++ src/components/partial/Trailer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactPlayer from "react-player";
 import { useSelector } from "react-redux";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 function Trailer() {
   const { pathname } = useLocation();
@@ -9,10 +9,21 @@ function Trailer() {
 
   const ytvideo = useSelector((state) => state[category].info.videos);
 
-  console.log(ytvideo);
+  // console.log(ytvideo);
+
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
   return (
     <>
       <div className="bg-[rgba(0,0,0,.9)] absolute top-0 left-0 z-[100] w-screen h-screen flex item-center justify-center">
+      <Link
+          onClick={goBack}
+          className="absolute top-[5%] right-[5%] text-white hover:text-[#6556CD] ri-close-fill text-3xl"
+        ></Link>
         <ReactPlayer
           height={600}
           width={1000}
