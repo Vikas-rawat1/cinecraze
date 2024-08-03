33d544998293bb006aa1a0985188b3be6d6132aa
vikas <rawatvikas689@gmail.com>
2024-08-03 06:28:27 +0530

    commit

--- src/components/partial/Topnav.jsx
+++ src/components/partial/Topnav.jsx
@@ -23,22 +23,26 @@ function Topnav() {
   }, [query]);
   return (
     <>
-      <div className="w-[80%] h-[10vh] relative flex items-center m-auto ">
-        <i className="text-zinc-400 text-xl ri-search-line mx-5"></i>
-        <input
-          onChange={(e) => setQuery(e.target.value)}
-          value={query}
-          type="text"
-          placeholder="Search Anything"
-          className="w-[50%] text-zinc-200 mx-1 p-5 outline-none border-none bg-transparent"
-        />
-        {query.length > 0 && (
-          <i
-            onClick={() => setQuery("")}
-            className=" text-zinc-400 text-3xl  ri-close-fill"
-          ></i>
-        )}
-
+      <div className="w-full lg:w-[80%] h-[10vh] relative  items-center m-auto flex  ">
+        <div className="lg:hidden ">
+          <i className="text-[#6556CD] ri-menu-line text-2xl"></i>
+        </div>
+        <div>
+          <i className="text-zinc-400 text-xl ri-search-line lg:mx-5 mx-2"></i>
+          <input
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            type="text"
+            placeholder="Search Anything"
+            className="lg:w-[60%] text-zinc-200 lg:mx-1 lg:p-5 outline-none border-none bg-transparent"
+          />
+          {query.length > 0 && (
+            <i
+              onClick={() => setQuery("")}
+              className=" text-zinc-400 text-3xl  ri-close-fill"
+            ></i>
+          )}
+        </div>
         <div className="z-[100] absolute w-[60%] max-h-[50vh] bg-zinc-200 top-[90%] left-[2%] overflow-auto">
           {searches.map((s, index) => (
             <Link
