c012118d7b33e973b4f3f00d420ad501821e5d34
vikas <rawatvikas689@gmail.com>
2024-08-18 20:18:22 +0530

    commit

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -42,7 +42,7 @@ function Popular() {
       setPopular([]);
       GetPopular();sdf
     }sdfsdfsdf
-  };wersdfsdfsdfdsaasdsasdfsvcvdsdffsdf
+  };wersdfsdfsdfdsaasdssdfasdfsvcvdsdffsdf
 sf
   useEffect(() => {
     refershHandler();
