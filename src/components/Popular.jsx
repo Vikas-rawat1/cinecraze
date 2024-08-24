f76d197d77353b00d74f3ee099dbaaa31e05cdbc
vikas <rawatvikas689@gmail.com>
2024-08-24 19:46:03 +0530

    commit

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -37,7 +37,7 @@ function Popular() {
   const refershHandler = () => {
     if (popular.length === 0) {
       GetPopular();
-    } else {
+    } else {kljk
       setPage(1);
       setPopular([]);d
       GetPopular();
